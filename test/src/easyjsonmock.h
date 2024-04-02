296168f698004dd070a6251e5bbd54ee0ac0ecea
wilfrantz <wilfrantz@outlook.com>
2024-04-01 22:19:53 -0400

    Starts unit tests from scratch.

--- test/src/easyjsonmock.h
+++ test/src/easyjsonmock.h
@@ -3,58 +3,30 @@
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
-
-#include "easyjson.h"
-
-using ::testing::AtLeast;
+#include <easyjson.h>
 
 namespace easyjson
 {
     class EasyJsonMock : public ::testing::Test
     {
     public:
-        EasyJsonMock() : easyjson(new EasyJsonCPP()) {}
-        ~EasyJsonMock() { delete easyjson; }
-
-        void loadConfigurationMock()
-        {
-            easyjson->loadConfiguration();
-        }
-
-        void parseArrayObjectMock(const Json::Value &root)
-        {
-            easyjson->parseArrayObjectData(root);
-        }
-
-        void validateRootObjectMock(const Json::Value &root)
-        {
-            easyjson->validateRootObject(root);
-        }
-
-        void parseArrayMemberMock(const std::string &member, const Json::Value &arrayValue)
-        {
-            easyjson->parseArrayMemberData(member, arrayValue);
-        }
-
-        void parseObjectMemberMock(const std::string &member, const Json::Value &objectValue)
-        {
-            easyjson->parseObjectMemberData(member, objectValue);
-        }
-
-        void processMemberMock(const std::string &member, const std::string &key, const Json::Value &value)
-        {
-            easyjson->processMemberData(member, key, value);
-        }
+        EasyJsonMock() : easy(new EasyJsonCPP()) {}
 
         // Mock methods
-        MOCK_METHOD(void, validateRootObject, (const Json::Value &root));
-        MOCK_METHOD(void, parseArrayObjectData, (const Json::Value &root));
-        MOCK_METHOD(void, parseArrayMemberData, (const std::string &member, const Json::Value &arrayValue));
-        MOCK_METHOD(void, parseObjectMemberData, (const std::string &member, const Json::Value &objectValue));
+        // MOCK_METHOD(void, loadConfiguration, ());
+        // MOCK_METHOD(void, parseArrayObjectData, (const Json::Value &root));
+        // MOCK_METHOD(void, validateRootObject, (const Json::Value &root));
+        // MOCK_METHOD(void, parseArrayMemberData, (const std::string &member, const Json::Value &arrayValue));
+        // MOCK_METHOD(void, parseObjectMemberData, (const std::string &member, const Json::Value &objectValue));
         MOCK_METHOD(void, processMemberData, (const std::string &member, const std::string &key, const Json::Value &value));
 
+        ~EasyJsonMock() { delete easy; }
+
     protected:
-        EasyJsonCPP *easyjson;
+        EasyJsonCPP *easy;
+
+        void SetUp() override {}
+        void TearDown() override {}
     };
 } // namespace easyjson
 
